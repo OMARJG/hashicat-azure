923db08aa4faaa4f547eb4cc7270283b3fc50603
AzureForAll <OAGOM12@GMAIL.COM>
2023-10-27 00:51:05 -0600

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,33 +1,6 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-
-  tags = {
-    environment = "Production"
-  }
+module "network" {
+  source               = "Azure/network/azurerm"
+  version              = "3.5.0"
+  resource_group_name  = "your-resource-group-name"  # Replace with your resource group name
+  use_for_each         = false  # Set to true if you want to create multiple resource instances
 }
